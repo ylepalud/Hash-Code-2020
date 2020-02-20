98bf39c3dd336425b77389b55dc37a87edcb97f7
e179070r <OOnugoh12>
2020-02-20 20:19:34 +0100

    Correction scoring

--- scoring.py
+++ scoring.py
@@ -1,26 +1,26 @@
 from model import Library
 
 
-def total_score_library(library : Library):
+def total_score_library(library: Library):
     total = 0
     for i in library.book_list:
         total += i.score
     return total
 
 
-def efficiency_library(library):
+def efficiency_library(library: Library):
     return total_score_library(library) / ((library.books_per_day * library.number_of_books) + library.signup_time)
 
 
-def efficiency_remaining_days(days, library):
+def efficiency_remaining_days(days: int, library: Library):
     score = 0
-    days_of_scan = days-library.signup_time
-    books_scanned = days_of_scan*library.books_per_day
+    days_of_scan = days - library.sign_up_days
+    books_scanned = days_of_scan * library.books_per_day
     list_books = library.get_books()
     for i in range(books_scanned):
         score += list_books[i].score
     return score
 
 
-def effeciency_on_given_days(days, library):
+def efficiency_on_given_days(days: int, library: Library):
     return efficiency_remaining_days(days, library)
